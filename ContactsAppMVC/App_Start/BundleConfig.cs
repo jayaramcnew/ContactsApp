987bdf731348a0deb445ac31faae0159470b9706
Jayaram Chandrasekara Kumaran <jayaramc8491@gmail.com>
2017-03-18 20:11:10 -0400

    initial commit

---
+++ ContactsAppMVC/App_Start/BundleConfig.cs
@@ -0,0 +1,43 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+// Bundle Config is used to initialize all the needed js , css , bootstrap etc.
+
+namespace ContactsAppMVC
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/js/lib1/jquery.min.js"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/js/lib1/bootstrap.min.js"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+                        "~/js/lib1/jquery.dataTables.min.js",
+                        "~/js/lib1/dataTables.bootstrap.min.js",
+                        "~/js/lib1/dataTables.colReorder.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/css/bootstrap.min.css",
+                     "~/css/jquery.dataTables.min.css",
+                     "~/css/jquery.dataTables_themeroller.css",
+                      "~/css/dataTables.bootstrap.min.css",
+                      "~/css/colReorder.bootstrap.min.css",
+                      "~/css/site.css"));
+
+            /*            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+                        // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+                        bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                                    "~/Scripts/modernizr-*"));
+                                    */
+
+
+        }
+    }
+}
---
+++ ContactsAppMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContactsAppMVC.Startup))]
+namespace ContactsAppMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
