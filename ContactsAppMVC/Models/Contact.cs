178fabb39870edeeef6af5dc4ca75cd96988d43c
Jayaram Chandrasekara Kumaran <jayaramc8491@gmail.com>
2017-03-19 20:42:18 -0400

    Model Added to Project

---
+++ ContactsAppMVC/Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactsAppMVC.Models
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+        public Guid UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhonePrimary { get; set; }
+        public string PhoneSecondary { get; set; }
+        public DateTime Birthday { get; set; }
+        public string StreetAddress1 { get; set; }
+        public string StreetAddress2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+
+    }
+}
